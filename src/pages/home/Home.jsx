d9fdc0ec38b9a472116ceab531b7095c3cd93387
Nitin <nitin@Nitins-MacBook-Pro.local>
2022-08-02 02:34:37 +0530

    Home pgae done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table";
 import "react-circular-progressbar/dist/styles.css";
 
 const Home = () => {
@@ -24,6 +25,7 @@ const Home = () => {
         </div>
         <div className="listContainer">
           <div className="listTitle">Latest Transactions</div>
+          <Table />
         </div>
       </div>
     </div>
